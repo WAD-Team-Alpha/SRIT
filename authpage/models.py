fba0ba3e0b96d848a9e1f78f58e8011e289609e1
surya0180 <teja8551@gmail.com>
2021-06-13 13:47:17 +0530

    Added functions

--- authpage/models.py
+++ authpage/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from .choices import *
 
 # Create your models here.
 
@@ -9,7 +10,7 @@ class Student(models.Model):
     usr_nm = models.CharField(max_length=100)
     roll_no = models.CharField(max_length=10)
     sem_no = models.IntegerField(default=1)
-    branch = models.CharField(default='CSE')
+    branch = models.CharField(choices=Department, default='CSE')
 
     def __str__(self):
         return self.roll_no
