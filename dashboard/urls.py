4e28a34f9ae033275672f90138fb25c9edab880c
surya0180 <teja8551@gmail.com>
2021-06-14 17:18:17 +0530

    Added previous marks backend

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,5 +4,6 @@ from dashboard import views
 urlpatterns = [
     path('home/', views.dashboard, name='home'),
     path('currentsem/', views.marks, name='currentsem'),
-    path('previoussem/', views.previousmarks, name='previoussem')
+    path('previoussem/', views.previousmarks, name='previoussem'),
+    path('previoussemnumber/', views.previoussemNo, name='previoussemnumber'),
 ]
