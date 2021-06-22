3200bc0558f429499b6622025f3e986aabbc25bf
surya0180 <teja8551@gmail.com>
2021-06-22 11:49:30 +0530

    Completed Forgot Password

--- authpage/urls.py
+++ authpage/urls.py
@@ -4,5 +4,7 @@ from authpage import views
 urlpatterns = [
     path('', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
-    path('signup/', views.signup, name='signup')
+    path('signup/', views.signup, name='signup'),
+    path('security_check/', views.securityCheck, name='security_code'),
+    path('forgot_password/', views.forgotPassword, name='forgot_password'),
 ]
